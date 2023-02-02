52a288d373d0276be4341b9241e996fc5f270e69
Lucy de Rojas <lucyderojas@gmail.com>
2023-02-02 09:00:01 +0000

    added sitemap and robots files

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// /** @type {import('next-sitemap').IConfig} */
+
+
+module.exports = {
+    siteUrl: 'https://beyond-beauty-by-chanelle.vercel.app/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
